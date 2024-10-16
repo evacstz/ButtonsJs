0a486b6ccd3ffb8aaad85a20985927bd5efaf675
evacstz <eva.cordeiro@aluno.ce.gov.br>
2024-10-16 19:37:30 -0300

    Calculadora de IMC

---
+++ text_input/script.js
@@ -0,0 +1,6 @@
+function calcular() {
+    let peso = document.getElementById('peso').value;
+    let altura = document.getElementById('altura').value;
+    // alert('O valor digitado foi:' + peso + altura);
+    // console.log('peso + altura');
+}
